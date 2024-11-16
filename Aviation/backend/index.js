5400bc92b4ea0fbc8f54fc55fb56a406cbf058e8
bpreetham58 <bpreetham58@gmail.com>
2024-11-17 00:16:27 +0530

    backend

---
+++ Aviation/backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require("cors");
+const itemRoutes = require('./routes/airRoute');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('MongoDB connection error:', error.message));
+
+app.use('/api/air', itemRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
